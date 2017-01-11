a6de4e8239cf0c0d9a3697493b1d6bd638af31dc
Taku <gcobs135102@gmail.com>
2017-01-11 16:10:02 +0800

    Done w/o revising README

--- test/main/main.test.js
+++ test/main/main.test.js
@@ -160,3 +160,32 @@ QUnit.module("function render", () => {
         });
     });
 });
+
+QUnit.module("DOM", () => {
+    QUnit.module("help", () => {
+        QUnit.test("opend by help button, closed by help button", (assert) => {
+            let done = assert.async();
+            $('.help-btn').trigger('click');
+            setTimeout(() => {
+                assert.ok($('.help-wrapper').css('display').indexOf('none') <= -1, "open correctly");
+                $('.help-btn').trigger('click');
+                setTimeout(() => {
+                    assert.ok($('.help-wrapper').css('display').indexOf('none') > -1, "close correctly");
+                    done();
+                }, 500);
+            }, 500);
+        });
+        QUnit.test("opend by help button, closed by help wrapper", (assert) => {
+            let done = assert.async();
+            $('.help-btn').trigger('click');
+            setTimeout(() => {
+                assert.ok($('.help-wrapper').css('display').indexOf('none') <= -1, "open correctly");
+                $('.help-wrapper').trigger('click');
+                setTimeout(() => {
+                    assert.ok($('.help-wrapper').css('display').indexOf('none') > -1, "close correctly");
+                    done();
+                }, 500);
+            }, 500);
+        });
+    });
+});
